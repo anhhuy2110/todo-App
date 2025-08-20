39a77b1c812af44607310a9cc338ca9a8f1d8c65
anhhuy2110 <bahuy21102001@gmail.com>
2025-08-20 20:49:45 +0700

    feature/handle-checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,65 @@
 import "./App.css";
 import Todoitem from "./components/Todoitem";
+import Sidebar from "./components/Sidebar";
+import { useRef, useState } from "react";
 
 function App() {
-  const todoList = ["Học bài", "Học bài 1", "Học bài 2", "Học bài 3"];
+  const [todoList, setTodoList] = useState([
+    { id: 1, name: "Học bài 1", isImportant: false, isCompleted: false },
+    { id: 2, name: "Học bài 2", isImportant: false, isCompleted: false },
+    { id: 3, name: "Học bài 3", isImportant: false, isCompleted: true },
+    { id: 4, name: "Học bài 4", isImportant: true, isCompleted: false },
+    { id: 5, name: "Học bài 5", isImportant: false, isCompleted: false },
+  ]);
 
-  const todos = todoList.map((todo) => {
-    return <Todoitem name={todo} />;
+  const inputRef = useRef();
+  // console.log({inputRef});
+
+  const handleCheckbox = (todoId) => {
+    const newTodoList = todoList.map((todo) => {
+      if (todo.id === todoId) {
+        return { ...todo, isCompleted: !todo.isCompleted };
+      }
+      return todo;
+    });
+    setTodoList(newTodoList);
+  };
+
+  const todos = todoList.map((todo, index) => {
+    return (
+      <Todoitem
+        id={todo.id}
+        name={todo.name}
+        key={todo.id}
+        isImportant={todo.isImportant}
+        isCompleted={todo.isCompleted}
+        handleCheckbox={handleCheckbox}
+      />
+    );
   });
+
+  function addItem(e) {
+    if (e.key === "Enter") {
+      const value = e.target.value;
+      setTodoList([...todoList, { id: crypto.randomUUID(), name: value }]);
+      inputRef.current.value = "";
+    }
+  }
 
   return (
     <div className="container">
       <input
+        ref={inputRef}
         type="text"
         name="add-new-task"
         className="task-input"
         alt="Add new task"
         placeholder="Add new task"
+        onKeyDown={addItem}
       />
 
       <div>{todos}</div>
+      {/* <Sidebar /> */}
     </div>
   );
 }
--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Todoitem = (propps) => {
   return (
     <div className="todo-item">
-      <p className="todo-item-text">{propps.name}</p>
+      <div className="todo-item-ifoLeft">
+        <input type="checkbox" checked={propps.isCompleted} onChange={() => {propps.handleCheckbox(propps.id)}}/>
+        <p className="todo-item-text">{propps.name}</p>
+      </div>
+      {propps.isImportant && <p>⭐</p>}
     </div>
   );
 };
